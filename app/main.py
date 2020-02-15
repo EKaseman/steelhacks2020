afc26f907fa1304008f4362f5ea55ba2b8e311f5
Mark Filus <markfilus@pitt.edu>
2020-02-15 00:58:25 -0500

    Clean up syntax

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+
 app = Flask(__name__)
 
 
@@ -9,8 +10,6 @@ def home():
     return render_template("base.html")
 
 
-
-
 if __name__ == "__main__":
     # Only for debugging while developing
-    app.run(host='0.0.0.0', debug=True, port=80)
+    app.run(host="0.0.0.0", debug=True, port=80)
