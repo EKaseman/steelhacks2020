07dbee83b4ad8b7a4682f76aabf5f67c342f2e4b
Mark Filus <markfilus@pitt.edu>
2020-02-15 09:53:10 -0500

    Delete utils again, revert reverted changes to a templates

--- app/utils.py
+++
@@ -1,6 +0,0 @@
-def recipe_diff(orignal, comparison):
-    """This function returns things added/removed in comparison recipe"""
-    return {
-        "added": "list of added in comparison recipe",
-        "subtracted": "list of subs.",
-    }
