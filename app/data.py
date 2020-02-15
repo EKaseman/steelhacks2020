ad389cc01c3bf90843a835e58b3dd6ac9cd8b111
Mark Filus <markfilus@pitt.edu>
2020-02-15 06:23:14 -0500

    Create the Recipe class

--- app/data.py
+++ app/data.py
@@ -1,110 +1,73 @@
-"""This dictionary contains everything we need """
+import pickle
+
+with open("data.pickle", "rb") as f:
+    full_recipe_data = pickle.load(f)
+
+
+# restructure the data
+# there are 8 recipes in the data with multi part instructions we are only
+# showing the first part for simplicity 238 recipes have one section of steps
 recipe_data = {
-    "0": {
-        "name": "chicken parmesan",
+    recipe["id"]: {
+        "steps": [step["step"] for step in recipe["analyzedInstructions"][0]["steps"]],
+        "name": recipe["title"],
+        "image": recipe["image"],
+        "likes": recipe["aggregateLikes"],
         "ingredients": {
-            [
-                "2 large eggs",
-                "1 tablespoon garlic",
-                "2 tablespoons parsley",
-                "salt and pepper",
-                "3 large chicken breasts halved",
-                "1 cup Panko breadcrumbs",
-                "1/2 cup breadcrumbs",
-                "1/2 cup fresh grated parmesan cheese",
-                "1 teaspoon garlic",
-                "1/2 cup olive oil for frying",
-                "1 tablespoon olive oil",
-                "1 large onion chopped",
-                "2 teaspoons minced garlic",
-                "14 ounces (400 g) tomato puree",
-                "Salt and pepper to taste",
-                "1 teaspoon dried Italian herbs",
-                "8 ounces (250 g) mozzarella cheese sliced or shredded",
-                "1/3 cup fresh shredded parmesan cheese",
-                "2 tablespoons fresh chopped basil",
-            ]
+            ing["id"]: {
+                "name": ing["name"],
+                "string": ing["originalString"],
+                "amount": ing["amount"],
+                "unit": ing["unit"],
+            }
+            for ing in recipe["extendedIngredients"]
         },
-        "instructions": {
-            "step1": "eheat oven 430°F | 220°C. Lightly grease an oven tray (or baking dish) with non stick cooking oil spray; set aside.",
-            "step2": "Whisk together eggs, garlic, parsley, salt and pepper in a shallow dish. Add chicken into the egg, rotating to evenly coat each fillet in the mixture. Cover with plastic wrap and allow to marinate for at least 15 minutes (or overnight night if time allows for a deeper flavour).",
-            "step3": "When chicken is ready for cooking, mix bread crumbs, Parmesan cheese and garlic powder together in a separate shallow bowl. Dip chicken into the breadcrumb mixture to evenly coat.",
-            "step4": "Heat oil in a large skillet over medium-high heat until hot and shimmering. Fry chicken until golden and crispy, (about 4-5 minutes each side).",
-            "step5": "Place chicken on prepared baking tray / dish and top each breast with about 1/3 cup of sauce (sauce recipe below). Top each chicken breast with 2-3 slices of mozzarella cheese and about 2 tablespoons parmesan cheese. Sprinkle with basil or parsley. ",
-            "step6": "Bake for 15-20 minutes, or until cheese is bubbling and melted, and the chicken is completely cooked through.",
-            "step7": "Heat oil in a medium-sized pot. Fry onion until transparent (about 3 minutes), then add the garlic until fragrant (about 30 seconds).",
-            "step8": "Add the tomato puree, salt and pepper to taste, Italian herbs and sugar (If using). Cover with lid to simmer for about 8 minutes, or until sauce has thickened slightly. Taste test and adjust salt and pepper, if needed.",
-        },
-        "description": "Deliciousness in a bag",
-        "time": "40 minutes",
-        "related": ["None In The Universe"],
-        "likes": 0,
-        "picture": "https://hips.hearstapps.com/vidthumb/images/delish-chicken-parm-still006-1553287552.jpg",
-        "tags": [],
-    },
-    "1": {
-        "name": "Baked Eggplant Parmesan",
-        "ingredients": {
-            [
-                "2 medium eggplants, sliced into 1/2-inch thick rounds",
-                "1 1/2 cups Panko breadcrumbs",
-                "1 Tablespoon Italian seasoning, homemade or store-bought",
-                "2 eggs (or 1 egg + 2 egg whites)",
-                "1 (25 ounce) jar DeLallo Tomato Basil Pomodoro Fresco Tomato Sauce",
-                "2 cups grated Mozzarella cheese (I used part-skim)",
-                "2/3 cup finely-grated Parmesan cheese",
-                "1/2 cup loosely-packed chopped or julienned fresh basil",
-                "salt",
-                "cooking spray (or olive oil in a Misto)",
-            ]
-        },
-        "instructions": {
-            "step1": "If you have extra time and would like to remove some of the bitterness of the eggplant, sprinkle each round with a pinch of salt.  Then place the rounds in a colander in the sink to drain, or place them on a few paper towels for about 30 minutes.  Rinse the salt off with water, then proceed with the recipe.",
-            "step2": "Preheat oven to 425°F.  Prepare two baking sheets with parchment paper, and set aside.",
-            "step3": "In a shallow bowl, whisk together Panko breadcrumbs, Italian seasoning and 1 teaspoon salt until combined.  In a separate bowl, whisk the eggs until smooth.  Dip an eggplant round on both sides in the whisked egg mixture, then immediately dip it in the breadcrumb mixture until the eggplant is completely coated, then set on a parchment-covered baking sheet.  Repeat with the remaining eggplant rounds until they are all evenly spaced on the baking sheets.  Bake for 20 minutes, flipping once halfway through, until the breadcrumbs are toasted and slightly golden.  Remove from the oven and set aside.",
-            "step4": "Spread 1/2 cup tomato sauce evenly over the bottom of an 11 x 8-inch baking dish.  Place half of the eggplant in a (mostly) even layer along the bottom of the baking dish.  Spread an additional 1 cup of tomato sauce evenly over the eggplant.  Then sprinkle 1 cup Mozzarella cheese evenly over the sauce, followed by 1/3 cup Parmesan cheese, followed by 1/4 cup of the fresh basil.  Repeat with another layer of the remaining eggplant, then tomato sauce, then Mozzarella, then Parmesan cheese.",
-            "step5": "Bake for 15-20 minutes until the cheese is melted and starts to turn slightly golden around the edges, and the sauce is bubbly.  Remove and sprinkle with the remaining basil.  Serve immediately.",
-        },
-        "description": "A healthy alternative to your favorite dish",
-        "time": "50 minutes",
-        "related": ["Chicken Parmesan"],
-        "likes": 0,
-        "picture": "https://www.gimmesomeoven.com/wp-content/uploads/2015/07/Eggplant-Parmesan-4.jpg",
-        "tags": [],
-    },
-    "2": {
-        "name": "Gluten-Free Chicken Parmesan",
-        "ingredients": {
-            [
-                "3 tablespoons olive oil plus more for greasing",
-                "3 tablespoons your preferred gluten-free flour mix",
-                "Kosher salt and freshly ground pepper",
-                "1 egg",
-                "2 teaspoons gluten-free Dijon mustard",
-                "2/3 cup gluten-free panko breadcrumbs",
-                "6 tablespoons finely grated Parmesan cheese divided",
-                "2 boneless skinless chicken cutlets",
-                "11/2 cups (12 ounces) your preferred jarred gluten-free marinara sauce",
-                "2/3 cup about 2 ounces shredded mozzarella cheese",
-                "1/2 cup lightly packed fresh basil leaves",
-            ]
-        },
-        "instructions": {
-            "step1": "Heat the oven to 350°F. Lightly grease a large, ovenproof frying pan or 9 by 13-inch baking dish.",
-            "step2": "In a small bowl, combine the flour, 1⁄2 teaspoon salt, and 1⁄4 teaspoon pepper.",
-            "step3": "In another small bowl, whisk together the egg, mustard, and 2 teaspoons water.",
-            "step4": "In a third small bowl, combine the breadcrumbs, 4 tablespoons of the Parmesan cheese, and a few grinds of pepper.",
-            "step5": "One at a time, place each chicken cutlet between two pieces of plastic wrap and, using a mallet or the bottom of a frying pan, gently pound to an even 1⁄4-inch thickness.",
-            "step6": "Dredge each piece of chicken in the seasoned flour. Shake off the excess, then dunk the chicken into the egg mixture to coat. Finally, coat with the breadcrumb mixture, using your hands to pack the crumb mixture onto the chicken pieces. Reserve on a plate.",
-            "step7": "Line a plate with paper towels. Heat the 3 tablespoons olive oil in a large skillet over medium-high heat until the oil is shimmering. Add the chicken in one layer, in batches, if necessary. Cook until each side is golden brown and crispy, 2 to 3 minutes per side. Transfer the cooked chicken to the prepared plate and sprinkle with salt.",
-            "step8": "Spread half the marinara sauce in the bottom of the prepared baking dish. Layer the chicken on top of the sauce. Pour the remaining sauce over the chicken, top with the mozzarella cheese and the remaining 2 tablespoons Parmesan cheese, and bake until the cheese is golden and bubbly, 20 to 25 minutes.",
-            "step9": "Chop the fresh basil leaves, sprinkle them over the chicken Parmesan, and serve.",
-        },
-        "description": "A gluten-free alternative to your favorite dish",
-        "time": "45 minutes",
-        "related": ["Chicken Parmesan"],
-        "likes": 0,
-        "picture": "https://i2.wp.com/gffmag.com/wp-content/uploads/2018/12/WW_Winter19_Chicken-Parm-e1553544222229.jpg?w=1200&ssl=1",
-        "tags": [],
-    },
+    }
+    for recipe in full_recipe_data
 }
+
+
+class Recipe:
+    def __init__(self, spoonacular_id):
+        self.id = spoonacular_id
+        rdict = recipe_data[int(spoonacular_id)]
+
+        self.image = rdict["image"]
+        self.ingredients = rdict["ingredients"]
+        self.likes = rdict["likes"]
+        self.name = rdict["name"]
+        self.steps =rdict["steps"]
+
+    def _diff_ingredients(self, r):
+        """ returns ingredients that are added in r, and ingredients that are
+        removed from this recipe and present in r """
+        our_ings = set(self.ingredients)
+        r_ings = set(r.ingredients)
+
+        # exclusive to r
+        added = {id:r.ingredients[id] for id in r_ings.difference(our_ings)}
+
+        # exclusive to this recipe
+        removed = {id:self.ingredients[id] for id in our_ings.difference(r_ings)}
+
+        return added, removed
+
+    def ingredients_added_in(self, other):
+        """Returns a list of ingredient names exclusive to other"""
+        added, _ = self._diff_ingredients(other)
+        return [d["name"]   for d in added.values() ]
+
+    def ingredients_removed_from(self, other):
+        """Returns a list of ingredient names exclusive to this recipe"""
+        _, removed = self._diff_ingredients(other)
+        return [d["name"]   for d in removed.values() ]
+
+    def similarity(self, other):
+        """Returns the length of the set of the symmetric difference, the total
+        number of items not in common"""
+
+        our_ings = set(self.ingredients)
+        other_ings = set(other.ingredients)
+        return len(our_ings.symmetric_difference(other_ings))
+
+
--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from data import Recipe
 
 app = Flask(__name__)
 
@@ -10,23 +11,6 @@ def home():
     return render_template("base.html")
 
 
-def isDiff(first, second):
-    sames = 0
-    num = 0
-    flag = true
-    for each in first:
-        num = num + 1
-        for each2 in second:
-            if first["ingredients"][each] == second["ingredients"][each2]:
-                sames = sames + 1
-    if sames == len(second):
-        sames = sames - 1
-    sames = sames / len(first)
-    if sames > 70:
-        flag = true
-    else:
-        flag = false
-    return flag
 
 
 if __name__ == "__main__":
