98996e4e7102e119128389a2cfd5dbb9af1594a3
Zach Sokoloff <zachsokoloff@gmail.com>
2020-02-15 00:54:23 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,8 +7,23 @@ def hello():
     return "Hello World from Flask in a uWSGI Nginx Docker container with \
      Python 3.7 (from the example template)"
 
-def 
-
+def isDiff(first,second):
+    sames = 0
+    num = 0
+    flag = true
+    for each in first:
+        num = num + 1
+        for each2 in second:
+            if (first["ingredients"][each] == second["ingredients"][each2]):
+                sames = sames + 1
+    if (sames == len(second)):
+        sames = sames - 1
+    sames = sames / len(first)
+    if (sames > 70):
+        flag = true
+    else:
+        flag = false
+    return flag
 
 
 if __name__ == "__main__":
