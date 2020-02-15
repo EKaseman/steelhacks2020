18e19f6c3e575b04ab89686ad5e43cfc920dc44a
Zach Sokoloff <zachsokoloff@gmail.com>
2020-02-14 22:27:27 -0500

    jh

--- app/data.py
+++ app/data.py
@@ -2,13 +2,26 @@
 """This dictionary contains everything we need """
 recipe_data = {
     int(): {
-        "name":str(),
-        "ingredients":{"name":"amount"},
-        "description":str(),
-    }
+        "name":"",
+        "ingredients": {["2 large eggs","1 tablespoon garlic","2 tablespoons parsley","salt and pepper","3 large chicken breasts halved","1 cup Panko breadcrumbs",""]},
+        "instructions": {"step1":"Add one cup of olives to the ice cream"},
+        "description": str(),
+        "time":0,
+        "related": [],
+        "likes": 0,
+        "picture": "cat.gif",
+        "tags": []
+    },
+}
+    
 }
 
 my_id=0
 """prints every ingredient in recipe 0"""
 for ingredient in recipe_data[my_id]["ingredients"]:
     print(ingredient)
+
+
+
+
+
--- app/main.py
+++ app/main.py
@@ -7,6 +7,9 @@ def hello():
     return "Hello World from Flask in a uWSGI Nginx Docker container with \
      Python 3.7 (from the example template)"
 
+def 
+
+
 
 if __name__ == "__main__":
     # Only for debugging while developing
